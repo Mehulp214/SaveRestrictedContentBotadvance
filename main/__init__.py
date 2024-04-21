3e7b4c59449a814ffd8f0256c5cf4c9c8a44c671
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-04-21 21:10:10 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -11,14 +11,20 @@ import logging, time, sys
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-# variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+#API_ID = config("API_ID", default=None, cast=int)
+#API_HASH = config("API_HASH", default=None)
+#BOT_TOKEN = config("BOT_TOKEN", default=None)
+#SESSION = config("SESSION", default=None)
+#FORCESUB = config("FORCESUB", default=None)
+#AUTH = config("AUTH", default=None, cast=int)
 
+API_ID=23476439
+API_HASH="1626e884119a29dbccbb78e39b48128f"
+BOT_TOKEN="7079929259:AAEC6jQzSzAPolYhL4nwExldNlIKf4sRcjU"
+SESSION="
+BQEDKZgAlnJHauRnpOhFry9WVo4CVGaaFHhirizMnvwciqkgjGRXKdX_tZ-7_6NAr-Ysb-vOl2xCMX2owrJOCIDXQVh_pAuOJQI15iMVBYel3jXmurhTPwcp39cGlX8Av8iF8xRyqrRjnx1vm42h1hUFXnd-6y-rOvid91Z4BdNCJR4mTwJgB5gA6GPwt4qDf2NRp01WsTE_TpVnfBvyiPVXXTxU1VIjrT6HprxUnG9qKeCxgqJ7S-_-u6efBytwFwah9WLccVC3XBTad0CcbXXaFLCoVQonGDU0DgCO42mQ3TZIWmgss84SNb6sAiexEQi3vNCC3LrG7sewESPqRAsfor_DuAAAAAFQUsvEAA"
+FORCESUB="MehulBots"
+AUTH=5434500969
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
 userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
